b03d65d274e45078145dae0655f302bac607190c
Henrik K <henrik@m.example.net>
2024-01-15 16:40:03 +0100

    Introduce the telemetry server

--- terraform/libvirt_cloud/README.md
+++ terraform/libvirt_cloud/README.md
@@ -4,5 +4,9 @@
 * virsh --connect=qemu+ssh://vmadm@homelab1/system list
 * virsh --connect=qemu+ssh://vmadm@homelab1/system domifaddr csgo_vm
 * update the haproxy.cfg
+  * sudo haproxy -c -f haproxy.cfg
+  * sudo cp haproxy.cfg /etc/haproxy/haproxy.cfg
+  * sudo systemctl restart haproxy
+  * systemctl status haproxy
 * ssh -i tf-cloud-init -p 6122 ansible@homelab1
 * Install and update the CS2 server
---
+++ terraform/libvirt_cloud/telemetry_server.tf
@@ -0,0 +1,56 @@
+# https://registry.terraform.io/providers/dmacvicar/libvirt/latest/docs/resources/cloudinit
+data "template_file" "telemetry_user_data" {
+  template = file("./telemetry_cloud-init-actions.yaml")
+
+  # left hand var names are the var names used in the cloud-init yaml.
+  vars = {
+    ansible_ssh_public_key = file(var.ansible_ssh_public_key_filename)
+    csgo_client_access_password = var.csgo_client_access_password
+    server_name = var.server_name
+  }
+}
+
+resource "libvirt_cloudinit_disk" "telemetry_cloudinit" {
+  name      = "telemetry_cloudinit.iso"
+  user_data = data.template_file.telemetry_user_data.rendered
+}
+
+# Defining VM Volume
+resource "libvirt_volume" "telemetry-qcow2" {
+  name = "telemetry.qcow2"
+  pool = "default" # List storage pools using virsh pool-list
+  source = "/var/ubuntu_jammy_cloudimg.qcow2"
+  format = "qcow2"
+}
+
+# Define KVM domain to create
+resource "libvirt_domain" "telemetry-vm" {
+  name   = "csgo_telemetry_vm"
+  memory = "4096"
+  vcpu   = 4
+
+  cloudinit = libvirt_cloudinit_disk.telemetry_cloudinit.id
+
+  network_interface {
+    network_name = "default" # List networks with virsh net-list
+    bridge = "virbr0"
+    wait_for_lease = true
+  }
+
+  disk {
+    volume_id = "${libvirt_volume.telemetry-qcow2.id}"
+  }
+
+  console {
+    type = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type = "spice"
+    listen_type = "address"
+    autoport = true
+  }
+}
+
